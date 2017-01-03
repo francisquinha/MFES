2415d33b588ac410fdbe676ed3f4962605a1a72d
Tiago Galvão <tiagogalvao90@gmail.com>
2017-01-03 11:28:35 +0000

    Remove Cell Class

--- tetris.vdm/generated/java/src/tetris/vdm/Board.java
+++ tetris.vdm/generated/java/src/tetris/vdm/Board.java
@@ -7,23 +7,24 @@ import org.overture.codegen.runtime.*;
 public class Board {
   private String print_startTag = "▕";
   private String print_endTag = "▏\n";
-  private String print_bottomLine = " ▔▔▔▔▔▔▔▔▔▔▔▔";
-  private String print_emptyCell = "░";
+  private String print_bottomLine = " ▔▔▔▔▔▔▔▔▔▔▔";
+  private String print_empty_cell = "░";
   private String print_filledcell = "█";
   private String print_board = "";
-  private Number totalRows = 11L;
-  private Number totalColumns = 11L;
+  private Number totalRows = 20L;
+  private Number totalColumns = 10L;
   private Number iterator = 0L;
   private VDMMap matrix = MapUtil.map();
   private VDMMap tempMatrix = MapUtil.map();
-  private Number emptyCell = 0L;
-  private Number forMoveCell = 1L;
-  private Number filledCell = 2L;
-  private Number tempCell = 9L;
+  private Number empty_cell = 0L;
+  private Number blocked_cell = 1L;
+  private Number move_cell = 111L;
+  private Number pieceOperation = 0L;
 
   public void cg_init_Board_1() {
 
     this.initBoard("matrix");
+    tempMatrix = Utils.copy(matrix);
   }
 
   public Board() {
@@ -33,36 +34,36 @@ public class Board {
 
   public void initBoard(final String option) {
 
-    {
-      if (Utils.equals(option, "matrix")) {
-        long toVar_2 = totalRows.longValue();
+    if (Utils.equals(option, "matrix")) {
+      long toVar_2 = totalRows.longValue();
 
-        for (Long x = 0L; x <= toVar_2; x++) {
-          long toVar_1 = totalColumns.longValue();
+      for (Long i = 0L; i <= toVar_2; i++) {
+        long toVar_1 = totalColumns.longValue();
 
-          for (Long y = 0L; y <= toVar_1; y++) {
-            matrix =
-                MapUtil.override(
-                    Utils.copy(matrix), MapUtil.map(new Maplet(SeqUtil.seq(x, y), 0L)));
-          }
+        for (Long j = 0L; j <= toVar_1; j++) {
+          matrix =
+              MapUtil.override(Utils.copy(matrix), MapUtil.map(new Maplet(SeqUtil.seq(i, j), 0L)));
         }
-      } else if (Utils.equals(option, "tempMatrix")) {
-        long toVar_4 = totalRows.longValue();
+      }
 
-        for (Long x = 0L; x <= toVar_4; x++) {
-          long toVar_3 = totalColumns.longValue();
+    } else if (Utils.equals(option, "tempMatrix")) {
+      long toVar_4 = totalRows.longValue();
 
-          for (Long y = 0L; y <= toVar_3; y++) {
+      for (Long i = 0L; i <= toVar_4; i++) {
+        long toVar_3 = totalColumns.longValue();
+
+        for (Long j = 0L; j <= toVar_3; j++) {
+          if (!(Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), blocked_cell))) {
             tempMatrix =
                 MapUtil.override(
-                    Utils.copy(tempMatrix), MapUtil.map(new Maplet(SeqUtil.seq(x, y), 0L)));
+                    Utils.copy(tempMatrix), MapUtil.map(new Maplet(SeqUtil.seq(i, j), 0L)));
           }
         }
       }
     }
   }
 
-  public String getBoardPrint() {
+  public String getBoardPrint(final String option) {
 
     print_board = "\n";
     long toVar_6 = totalRows.longValue();
@@ -74,24 +75,32 @@ public class Board {
       for (Long j = 0L; j <= toVar_5; j++) {
         Boolean orResult_1 = false;
 
-        if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), forMoveCell)) {
+        if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
           orResult_1 = true;
         } else {
-          orResult_1 = Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), filledCell);
+          orResult_1 = Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), blocked_cell);
         }
 
         if (orResult_1) {
           print_board = print_board + print_filledcell;
         } else {
-          print_board = print_board + print_emptyCell;
+          print_board = print_board + print_empty_cell;
         }
       }
       print_board = print_board + print_endTag;
     }
+    if (Utils.equals(option, "printBoard")) {
+      IO.println(print_board + print_bottomLine);
+    }
+
     return print_board + print_bottomLine;
   }
 
-  public VDMMap getBoard() {
+  public VDMMap getBoard(final String option) {
+
+    if (Utils.equals(option, "printMatrix")) {
+      IO.println(this.matrix);
+    }
 
     return this.matrix;
   }
@@ -103,33 +112,164 @@ public class Board {
       final VDMSeq coord3,
       final VDMSeq coord4) {
 
-    Cell cell = new Cell();
+    {
+      if (Utils.equals(option, "set")) {
+        pieceOperation = move_cell;
+      } else if (Utils.equals(option, "remove")) {
+        pieceOperation = empty_cell;
+      }
+    }
+
     matrix =
-        cell.setPiece(
-            option,
-            Utils.copy(coord1),
-            Utils.copy(coord2),
-            Utils.copy(coord3),
-            Utils.copy(coord4),
-            Utils.copy(matrix));
+        MapUtil.override(
+            Utils.copy(matrix), MapUtil.map(new Maplet(Utils.copy(coord1), pieceOperation)));
+    matrix =
+        MapUtil.override(
+            Utils.copy(matrix), MapUtil.map(new Maplet(Utils.copy(coord2), pieceOperation)));
+    matrix =
+        MapUtil.override(
+            Utils.copy(matrix), MapUtil.map(new Maplet(Utils.copy(coord3), pieceOperation)));
+    matrix =
+        MapUtil.override(
+            Utils.copy(matrix), MapUtil.map(new Maplet(Utils.copy(coord4), pieceOperation)));
   }
 
-  public void cell_automaticallyMovePiece() {
+  public void checkBoard() {
 
-    Cell cell = new Cell();
-    this.initBoard("tempMatrix");
-    matrix = cell.automaticallyMovePiece(Utils.copy(tempMatrix), Utils.copy(matrix));
+    Boolean block = false;
+    long toVar_8 = totalRows.longValue();
+
+    for (Long i = 0L; i <= toVar_8; i++) {
+      long toVar_7 = totalColumns.longValue();
+
+      for (Long j = 0L; j <= toVar_7; j++) {
+        Boolean orResult_2 = false;
+
+        Boolean andResult_1 = false;
+
+        if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+          if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(totalRows, j))), move_cell)) {
+            andResult_1 = true;
+          }
+        }
+
+        if (andResult_1) {
+          orResult_2 = true;
+        } else {
+          Boolean andResult_2 = false;
+
+          if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+            if (Utils.equals(
+                ((Number) Utils.get(matrix, SeqUtil.seq(i.longValue() + 1L, j))), blocked_cell)) {
+              andResult_2 = true;
+            }
+          }
+
+          orResult_2 = andResult_2;
+        }
+
+        if (orResult_2) {
+          block = true;
+        }
+      }
+    }
+    if (Utils.equals(block, true)) {
+      long toVar_10 = totalRows.longValue();
+
+      for (Long i = 0L; i <= toVar_10; i++) {
+        long toVar_9 = totalColumns.longValue();
+
+        for (Long j = 0L; j <= toVar_9; j++) {
+          if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+            Utils.mapSeqUpdate(matrix, SeqUtil.seq(i, j), blocked_cell);
+          }
+        }
+      }
+    }
+
+    tempMatrix = Utils.copy(matrix);
+    long toVar_12 = totalRows.longValue();
+
+    for (Long i = 0L; i <= toVar_12; i++) {
+      long toVar_11 = totalColumns.longValue();
+
+      for (Long j = 0L; j <= toVar_11; j++) {
+        if (Utils.equals(((Number) Utils.get(tempMatrix, SeqUtil.seq(i, j))), move_cell)) {
+          Utils.mapSeqUpdate(tempMatrix, SeqUtil.seq(i, j), empty_cell);
+        }
+      }
+    }
+  }
+
+  public Boolean cell_automaticallyMovePiece() {
+
+    this.checkBoard();
+    long toVar_14 = totalRows.longValue();
+
+    for (Long i = 0L; i <= toVar_14; i++) {
+      long toVar_13 = totalColumns.longValue();
+
+      for (Long j = 0L; j <= toVar_13; j++) {
+        if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+          Utils.mapSeqUpdate(tempMatrix, SeqUtil.seq(i.longValue() + 1L, j), move_cell);
+        }
+      }
+    }
+    matrix = Utils.copy(tempMatrix);
+    return true;
   }
 
   public void cell_manuallyMovePiece(final String option, final Number numberOfClicks) {
 
-    Cell cell = new Cell();
-    this.initBoard("tempMatrix");
-    matrix =
-        cell.manuallyMovePiece(option, numberOfClicks, Utils.copy(tempMatrix), Utils.copy(matrix));
+    this.checkBoard();
+    if (Utils.equals(option, "moveRight")) {
+      long toVar_16 = totalRows.longValue();
+
+      for (Long i = 0L; i <= toVar_16; i++) {
+        long toVar_15 = totalColumns.longValue();
+
+        for (Long j = 0L; j <= toVar_15; j++) {
+          if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+            Utils.mapSeqUpdate(
+                tempMatrix, SeqUtil.seq(i, j.longValue() + numberOfClicks.longValue()), move_cell);
+          }
+        }
+      }
+
+    } else if (Utils.equals(option, "moveLeft")) {
+      long toVar_18 = totalRows.longValue();
+
+      for (Long i = 0L; i <= toVar_18; i++) {
+        long toVar_17 = totalColumns.longValue();
+
+        for (Long j = 0L; j <= toVar_17; j++) {
+          if (Utils.equals(((Number) Utils.get(matrix, SeqUtil.seq(i, j))), move_cell)) {
+            Utils.mapSeqUpdate(
+                tempMatrix, SeqUtil.seq(i, j.longValue() - numberOfClicks.longValue()), move_cell);
+          }
+        }
+      }
+    }
+
+    matrix = Utils.copy(tempMatrix);
   }
 
   public Boolean checkRow(final Number row) {
+
+    throw new UnsupportedOperationException();
+  }
+
+  public void setMatrixPosition(final VDMSeq position, final Number value) {
+
+    Utils.mapSeqUpdate(matrix, Utils.copy(position), value);
+  }
+
+  public Number getMatrixPosition(final VDMSeq position) {
+
+    return ((Number) Utils.get(matrix, position));
+  }
+
+  public void setGameOver() {
 
     throw new UnsupportedOperationException();
   }
@@ -143,8 +283,8 @@ public class Board {
         + Utils.toString(print_endTag)
         + ", print_bottomLine := "
         + Utils.toString(print_bottomLine)
-        + ", print_emptyCell := "
-        + Utils.toString(print_emptyCell)
+        + ", print_empty_cell := "
+        + Utils.toString(print_empty_cell)
         + ", print_filledcell := "
         + Utils.toString(print_filledcell)
         + ", print_board := "
@@ -159,14 +299,14 @@ public class Board {
         + Utils.toString(matrix)
         + ", tempMatrix := "
         + Utils.toString(tempMatrix)
-        + ", emptyCell := "
-        + Utils.toString(emptyCell)
-        + ", forMoveCell := "
-        + Utils.toString(forMoveCell)
-        + ", filledCell := "
-        + Utils.toString(filledCell)
-        + ", tempCell := "
-        + Utils.toString(tempCell)
+        + ", empty_cell := "
+        + Utils.toString(empty_cell)
+        + ", blocked_cell := "
+        + Utils.toString(blocked_cell)
+        + ", move_cell := "
+        + Utils.toString(move_cell)
+        + ", pieceOperation := "
+        + Utils.toString(pieceOperation)
         + "}";
   }
 }
--- tetris.vdm/generated/java/src/tetris/vdm/TestTetris.java
+++ tetris.vdm/generated/java/src/tetris/vdm/TestTetris.java
@@ -7,75 +7,73 @@ import org.overture.codegen.runtime.*;
 public class TestTetris extends TestCaseExtra {
   public VDMMap matrix = MapUtil.map();
   public String printBoard = "";
-  public Boolean endGame = false;
 
-  private void addPiece_test() {
+  private void initalMatrix_test(final Game game) {
 
-    Game game1 = new Game();
-    IO.println("# Test 1 - Add random piece; move the piece manually/automatically\n");
     IO.println("-> Initial matrix");
-    matrix = game1.board.getBoard();
-    IO.println(Utils.copy(matrix));
-    printBoard = game1.board.getBoardPrint();
-    IO.println(printBoard);
-    IO.println("-> Add piece on the matrix");
-    game1.board.cell_setPiece(
-        "set", SeqUtil.seq(0L, 2L), SeqUtil.seq(1L, 2L), SeqUtil.seq(2L, 2L), SeqUtil.seq(3L, 2L));
-    matrix = game1.board.getBoard();
-    IO.println(Utils.copy(matrix));
-    printBoard = game1.board.getBoardPrint();
-    IO.println(printBoard);
-    IO.println("-> Move piece automatically");
-    game1.board.cell_automaticallyMovePiece();
-    matrix = game1.board.getBoard();
-    IO.println(Utils.copy(matrix));
-    printBoard = game1.board.getBoardPrint();
-    IO.println(printBoard);
-    IO.println("-> Move piece automatically (second call)");
-    game1.board.cell_automaticallyMovePiece();
-    matrix = game1.board.getBoard();
-    IO.println(Utils.copy(matrix));
-    printBoard = game1.board.getBoardPrint();
-    IO.println(printBoard);
-    IO.println("-> Move piece manually (keyboard click simulation)");
-    game1.board.cell_manuallyMovePiece("moveRight", 8L);
-    matrix = game1.board.getBoard();
-    IO.println(Utils.copy(matrix));
-    printBoard = game1.board.getBoardPrint();
-    IO.println(printBoard);
+    matrix = game.board.getBoard("printMatrix");
+    printBoard = game.board.getBoardPrint("printBoard");
   }
 
-  private void timer_test() {
+  private void addPiece_test(
+      final Game game,
+      final VDMSeq coord1,
+      final VDMSeq coord2,
+      final VDMSeq coord3,
+      final VDMSeq coord4) {
+
+    IO.println("-> Add piece on the matrix");
+    game.board.cell_setPiece(
+        "set", Utils.copy(coord1), Utils.copy(coord2), Utils.copy(coord3), Utils.copy(coord4));
+    matrix = game.board.getBoard("printMatrix");
+    printBoard = game.board.getBoardPrint("printBoard");
+  }
+
+  private void automaticallyMovePiece_test(
+      final Game game, final Number totalTime, final Number step) {
 
     Timer timer = new Timer();
-    IO.print("\n# timer_test\n");
+    Boolean endLoop = false;
+    Boolean movePiece = true;
+    IO.println("-> Move piece automatically (dummy timer)");
     Boolean whileCond_1 = true;
     while (whileCond_1) {
-      whileCond_1 = !(endGame);
+      whileCond_1 = !(endLoop);
       if (!(whileCond_1)) {
         break;
       }
 
       {
         timer.incTimers();
-        if (Utils.equals(timer.getTimer("time"), 100000L)) {
-          IO.print("-> END \n");
+        if (Utils.equals(timer.getTimer("time"), 180000L * totalTime.longValue())) {
           timer.resetTimer("all");
-          endGame = true;
+          endLoop = true;
         }
 
-        if (Utils.equals(timer.getTimer("step"), 20000L)) {
-          IO.print("-> Matrix \n");
+        if (Utils.equals(timer.getTimer("step"), 4000L * step.longValue())) {
+          movePiece = game.board.cell_automaticallyMovePiece();
+          matrix = game.board.getBoard("printMatrix");
+          printBoard = game.board.getBoardPrint("printBoard");
           timer.resetTimer("step");
         }
       }
     }
   }
 
+  private void manuallyMovePiece_test(final Game game) {
+
+    IO.println("-> Move piece manually (keyboard click simulation)");
+    game.board.cell_manuallyMovePiece("moveRight", 8L);
+    matrix = game.board.getBoard("printMatrix");
+    printBoard = game.board.getBoardPrint("printBoard");
+  }
+
   public static void main() {
 
+    Game game = new Game();
     IO.print("\n##### TESTS #####\n");
-    new TestTetris().addPiece_test();
+    IO.println("# Test 1 - Add random piece; move the piece manually/automatically\n");
+    new TestTetris().initalMatrix_test(game);
   }
 
   public TestTetris() {}
@@ -87,8 +85,6 @@ public class TestTetris extends TestCaseExtra {
         + Utils.toString(matrix)
         + ", printBoard := "
         + Utils.toString(printBoard)
-        + ", endGame := "
-        + Utils.toString(endGame)
         + "}";
   }
 }
